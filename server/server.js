e494f2cea7a7a4dcad46b06afdc8ee7f2020440d
2gacem7 <gacemcode@gmail.com>
2021-02-05 15:23:57 +0100

    ajout du add et du get

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors')
 const db = require('./connection.js')
+const bodyParser = require('body-parser')
+const todoModel = require('./todo_schema')
 
 const app = express();
 app.use(cors())
@@ -9,3 +11,30 @@ app.listen(8080, () => {
  console.log("Server started on port 8080")
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/todo/add', async (req, res) => {  
+    try {
+    let newTodo = new todoModel();  
+    newTodo.title = req.body.title;  
+    newTodo.completed = false;  
+    await newTodo.save();
+    res.send('Bonjour')
+    }catch(err){
+        res.send(err)
+        console.error(err)
+    }
+
+})
+
+app.get('/todo/completed', async (req, res) => {  
+    let todos = await todoModel.find()   
+        try{
+            res.json(todos);
+            res.send('Salut') 
+        }catch(err){
+            res.send(err)
+            console.error(err)
+        }      
+    })
