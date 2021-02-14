249c23c75a8c2bd964c186e9c098d1715ba36b07
2gacem7 <gacemcode@gmail.com>
2021-02-14 18:28:43 +0100

    [ADDED] The remove button has been added, but i have to see why the completed checkbox don't be saved

--- client/src/pages/Todo.vue
+++ client/src/pages/Todo.vue
@@ -1,17 +1,25 @@
 <template>
 <div class="q-pa-md row items-start q-gutter-md">
+  <div class="q-gutter-md" style="max-width: 300px">
+      <q-input v-model="title" label="Task" />
+      <q-btn padding="xs lg"
+      color="primary"
+      icon="eco"
+      @click="addTodo"
+      :disabled="!title"> Add </q-btn>
+  </div>
   <div class="col-md-12 col-xs-12 text-center">
-    <div  v-for="todo in todos" :key="todo" class= "row justify-center" >
+    <div  v-for="(todo) in todos" :key="todo._id" class= "row justify-center notification">
       <div class="col-md-4 col-xs-12 text-center">
-            <div v-if="todo.completed===false">
 
+            <div v-if="todo.completed===false">
   <q-card
     class="my-card text-white col-md-4 col-xs-12 text-center"
     style="background: radial-gradient(circle, #4B5FF1 0%, #09198B 100%)"
   >
     <q-card-section>
       <div class="text-h6">
-        {{todo.title}}
+        {{todo.title}}  
         <q-checkbox v-model="todo.completed" color="teal" />
       </div>
     </q-card-section>
@@ -19,11 +27,14 @@
             </div>
   </div>
   <div class="col-md-4 col-xs-12 text-center">
-    <div v-if="todo.completed===true">
+    <div v-if="todo.completed===true">     
   <q-card
     class="my-card text-black my-card text-white col-md-4 col-xs-12 text-center"
     style="background: radial-gradient(circle, #DADFDA 0%, #858A85 100%)"
   >
+  <q-btn class="" @click="removeTodo(todo)"> 
+          <i class="material-icons"> delete</i>
+        </q-btn>
     <q-card-section>
       <div class="text-h6">
         {{todo.title}}
@@ -46,10 +57,11 @@ export default {
   name: 'App',
   data(){
     return {
-      todos: []
+      todos: [],
+      title:"",
     };
   },
-   async created() {
+  async mounted() {
     try {
       const response = await axios.get('http://localhost:8080/todo/completed')
       this.todos = response.data
@@ -57,7 +69,21 @@ export default {
     } catch (e) {
       this.errors.push(e)
     }
-  } 
+  },
+  methods: {
+    async addTodo() {
+            const response = await axios.post('http://localhost:8080/todo/add', {
+              title : this.title
+            });
+            this.todos.push(response.data);
+            this.title ="";
+            location.reload();
+    },
+    async removeTodo(todo){
+      await axios.delete('http://localhost:8080/todo/'+ todo._id)
+      this.todos.splice(todo, 1)
+    }
+  }
 }
 </script>
 
